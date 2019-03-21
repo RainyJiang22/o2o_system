9dd29641d811367bae33e27bd2b0020f89f664ba
jackytallow <3434481891@qq.com>
2019-03-21 11:16:58 +0800

    初步编写城市分类

---
+++ nuomi_system/application/admin/controller/City.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Jacky
+ * Date: 2019/3/21
+ * Time: 10:37
+ */
+
+namespace app\admin\controller;
+
+
+use think\Controller;
+
+class City extends Controller
+{
+
+    public function index(){
+        return $this->fetch();
+    }
+
+
+    /**
+     * @return mixed
+     * 添加城市
+     */
+    public function add(){
+        return $this->fetch();
+    }
+
+
+    /**
+     * 保存数据
+     */
+    public function save(){
+       // print_r($_POST);
+
+        
+    }
+}

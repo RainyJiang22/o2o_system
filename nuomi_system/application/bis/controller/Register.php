c787afb662700ece4da641820af4f2c838a6cba6
jackytallow <3434481891@qq.com>
2019-03-25 19:38:08 +0800

    商户平台模块初步搭建

---
+++ nuomi_system/application/bis/controller/Register.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Jacky
+ * Date: 2019/3/25
+ * Time: 19:32
+ */
+namespace app\bis\controller;
+use think\Controller;
+
+class Register extends Controller
+{
+
+    public function index(){
+        return $this->fetch();
+    }
+}

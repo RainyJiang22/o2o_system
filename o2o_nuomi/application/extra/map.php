5082129f576cf2f4f65cc2838f6453ce9258877f
jackytallow <3434481891@qq.com>
2019-04-09 18:57:58 +0800

    网站初步完成v1.0

---
+++ o2o_nuomi/application/extra/map.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * 地图相关配置文件
+ */
+return [
+    'ak' => 'd7axjbHf4SZHG2GB0SqHbRV5IdnkZnEL',
+    'baidu_map_url' => 'http://api.map.baidu.com/',
+    'geocoder' => 'geocoder/v2/',
+    'width' => 400,
+    'height' => 300,
+    'staticimage' => 'staticimage/v2',
+];

9e7ac85ce678d2204182c7b88f61a2eb2aee3491
jackytallow <3434481891@qq.com>
2019-04-09 18:56:17 +0800

    初步添加本地团购

---
+++ nuomi_system/application/bis/controller/Deal.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Jacky
+ * Date: 2019/4/9
+ * Time: 16:57
+ */
+
+namespace app\bis\controller;
+
+
+use think\Controller;
+
+class Deal extends Base
+{
+
+
+    public function index(){
+
+
+    }
+
+    /**
+     * 团购商品添加
+     */
+    public function add(){
+
+
+        //获取一级城市的数据
+        $citys = model('City')->getNormalCitysByParentId();
+        //获取一级栏目的数据
+        $categorys = model('Category')->getNormalCategoryByParentId();
+        return $this->fetch('',[
+            'citys' => $citys,
+            'categorys' => $categorys,
+        ]);
+    }
+}
